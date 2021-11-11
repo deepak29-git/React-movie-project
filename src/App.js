50af9e124cb62e3ad81fde2681536708f4e4e146
Deepak Goyal <dg887180@gmail.com>
2021-11-11 19:21:54 +0530

    deployed

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React,{ useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css"
 
-import Summary from "./Summary";
-import {Route,Routes} from 'react-router-dom'
-import Form from "./Form";
-import Home from "./Home"
+import Summary from "./Component/Summary";
+import { Route, Routes } from 'react-router-dom'
+import Form from "./Component/Form";
+import Home from "./Component/Home"
 
 
-export default function App(){
-  
-  const [apiData,setApiData]=useState("");
-  var serverlUrl="https://api.tvmaze.com/search/shows?q=all"
-  useEffect(()=>{
-    fetch(serverlUrl).then(response=>response.json())
-    .then(data=>{
-      setApiData(data)
-    })
-  },[serverlUrl])
-  
-  return(
+export default function App() {
+
+  const [apiData, setApiData] = useState("");
+  var serverlUrl = "https://api.tvmaze.com/search/shows?q=all"
+  useEffect(() => {
+    fetch(serverlUrl).then(response => response.json())
+      .then(data => {
+        setApiData(data)
+      })
+  }, [serverlUrl])
+
+  return (
     <div className="App">
       <Routes>
-        <Route path="/" exact element={<Home list={apiData}/>}/>
-        <Route path="/data/:id" element={<Summary list={apiData}/>}/>
-        <Route path="/form/:id" element={<Form list={apiData}/>}/>
+        <Route path="/" exact element={<Home list={apiData} />} />
+        <Route path="/data/:id" element={<Summary list={apiData} />} />
+        <Route path="/form/:id" element={<Form list={apiData} />} />
       </Routes>
-    </div> 
+    </div>
   )
 }
 
---
+++ src/Component/Form.js
@@ -0,0 +1,39 @@
+import { useParams } from "react-router"
+import "./Form.css"
+
+
+export default function Form({ list }) {
+    const { id } = useParams()
+    const data = list?.find(item => item.show.id == id)
+
+    function submitHandler(event) {
+        event.preventDefault()
+    }
+
+    return (
+        <div>
+            <form >
+                <h2>{data.show.name}</h2>
+                <img src={data.show.image.medium} alt="" />
+                <p>Language: {data.show.language}</p>
+                <p>Genres: {data.show.genres}</p>
+                <p>Ratings: {data.show.rating.average}</p>
+                <label htmlFor="">Url: </label>
+                <a style={{ textDecoration: "none", color: "blue" }} href={`${data.show.url}`}>{data.show.url}</a>
+
+
+                <h4>YOUR CONTACT DETAILS</h4>
+                <label htmlFor="name">NAME</label><br />
+                <input type="name" placeholder="Full Name" /><br />
+                <label htmlFor="email">E-MAIL</label><br />
+                <input type="email" placeholder="Email Address" name="" id="" /><br />
+                <label htmlFor="phone">PHONE</label><br />
+                <input type="phone" placeholder="Phone Number" name="" id="" /><br />
+
+                <button onClick={(event) => submitHandler(event)} style={{ color: "white" }}>Submit</button>
+
+
+            </form>
+        </div>
+    )
+}
